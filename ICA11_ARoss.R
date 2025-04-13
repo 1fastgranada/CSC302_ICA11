e319e82dfb2e758bf555193dd0b22d750794717b
Adam <1fastgranada@gmail.com>
2025-04-12 22:21:15 -0400

    Fixed typo

--- ICA11_ARoss.R
+++ ICA11_ARoss.R
@@ -2,11 +2,10 @@ library(ggplot2)
 library(tidyr)
 library(dplyr)
 
+# My Drive mounts to a strange place, but this works:
 flights = read.csv('I:/.shortcut-targets-by-id/1ehWwunuAo7CE1Vk2JYkUnQMmxh5pph3C/DATA/flights.csv')
 
 head(flights)
 
 ggplot(data=flights) + geom_line(aes(x=year, y=passengers, color=month)) + 
   xlab('Year') + ylab('Number of Passengers') + ggtitle('Number of Passengers by Month')
-
-flights%>%gather()
