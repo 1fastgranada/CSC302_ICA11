d36309709520757fca2c9791edc862b1921e7820
Adam <1fastgranada@gmail.com>
2025-04-12 22:19:31 -0400

    Add files via upload

---
+++ ICA11_ARoss.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+
+flights = read.csv('I:/.shortcut-targets-by-id/1ehWwunuAo7CE1Vk2JYkUnQMmxh5pph3C/DATA/flights.csv')
+
+head(flights)
+
+ggplot(data=flights) + geom_line(aes(x=year, y=passengers, color=month)) + 
+  xlab('Year') + ylab('Number of Passengers') + ggtitle('Number of Passengers by Month')
+
+flights%>%gather()
